02bae5dd4725469a306e406b0ccf4b08270fbd9f
Eric Jennings <eric.jennings.17@cnu.edu>
2020-04-04 16:48:42 -0400

    Included more example code from project description

--- app/src/main/java/com/example/android_project_3/MainActivity.java
+++ app/src/main/java/com/example/android_project_3/MainActivity.java
@@ -1,14 +1,35 @@
 package com.example.android_project_3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+    private SharedPreferences myPreference;
+    private SharedPreferences.OnSharedPreferenceChangeListener listener = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myPreference=PreferenceManager.getDefaultSharedPreferences(this);
+
+        listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            //Removed override statement since not part of example
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+                if (key.equals("listPref")) {
+                    //loadImage();
+                }
+            }
+        };
+    }
+
+    protected void processJSON(String result){
+        tv.setText(result);
     }
 }
